7d0959def8a3448b301c9109858f5887ffdadf68
Steve Schmitt <steves@ideablade.com>
2018-03-23 14:12:32 +0530

    Fix typescript compile error from @types/acorn

--- rollup/build-util.js
+++ rollup/build-util.js
@@ -27,16 +27,35 @@ function run(cmd, fn) {
     stdout && console.log('stdout: ' + stdout);
     stderr && console.log('stderr: ' + stderr);
     if (error !== null) {
-      console.log('rollup exec error: ' + error);
+      console.log('run exec error: ' + error);
     } else {
       fn();
     }
   });
 }
 
+/** replace a string in a file */
+function replaceInFile(filename, search, replace) {
+    fs.readFile(filename, 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        var result = data.replace(search, replace);
+        if (result === data) {
+            console.log("replaceInFile: no change to file " + filename);
+            return;
+        }
+
+        fs.writeFile(filename, result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+        console.log("replaceInFile: changed file " + filename);
+    });
+}
 
 module.exports = {
   isDifferent: isDifferent,
   getFileSize: getFileSize,
-  run: run
+  run: run,
+  replaceInFile: replaceInFile
 }
